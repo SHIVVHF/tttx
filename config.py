67416320dc142d1533ee96e14746e2106c3e9e0f
SHIVVHF <herokuha678@gmail.com>
2023-11-05 16:35:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5115275139:AAEJwqeGo0RmycATNb-1gBxUm9sRZYFXJlQ")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6274752151:AAFfCgIyZPbMl_bYIPoY8ry0WhvE3Gl5JPY")
     API_ID = int(os.environ["API_ID", "15052451"]
     API_HASH = os.environ["API_HASH", "dbf8fdfc66d7a1a9bf359c036409aa14"]
     AUTH_USERS = "1181522124)"
