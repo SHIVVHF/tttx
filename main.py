5f8ad2383529d3d33939a3c18df032ca372d43c4
SHIVVHF <herokuha678@gmail.com>
2023-11-05 16:05:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ from dotenv import load_dotenv
 load_dotenv()
 os.makedirs("./downloads", exist_ok=True)
 
-AUTH_USERS = 1112773045
-sudo_users = [-1112773045]
+AUTH_USERS = 1181522124
+sudo_users = [-1181522124]
 bot = Client(
     "bot",
     bot_token=os.environ.get("BOT_TOKEN"),
